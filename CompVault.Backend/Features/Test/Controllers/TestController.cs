a38dcdc49d2c14a4668035f26a533dbfb5cd1ec3
lynsoftware <dev@lynsoftware.com>
2026-03-10 21:51:54 +0100

    feat: lagt til en enkel emailtemplate og testcontroller for å teste emailtjenesten

---
+++ CompVault.Backend/Features/Test/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using CompVault.Backend.Controllers;
+using CompVault.Backend.Features.Test.Dtos;
+using CompVault.Backend.Infrastructure.Email;
+using CompVault.Backend.Infrastructure.Email.Templates;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompVault.Backend.Features.Test.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController(IEmailService emailService) : BaseController
+{
+    /// <summary>
+    /// Test endepunkt for å sjekke at epost fungerer
+    /// </summary>
+    /// <param name="request">TestEmailRequest med epost</param>
+    /// <returns>Status 200 Ok</returns>
+    [HttpPost("test-email")]
+    public async Task<ActionResult> TestEmailService([FromBody] TestEmailRequest request)
+    {
+        var template = EmailTemplates.SimpleText("Test tittel", "Test body");
+        var result = await emailService.SendAsync(request.RecipientEmail, template);
+        if (result.IsFailure)
+            HandleFailure(result);
+
+        return Ok();
+    }
+}
---
+++ CompVault.Backend/Infrastructure/Email/Templates/EmailTemplates.cs
@@ -0,0 +1,13 @@
+﻿using CompVault.Backend.Infrastructure.Email.Models;
+namespace CompVault.Backend.Infrastructure.Email.Templates;
+
+/// <summary>
+/// Generisk epost template som oppretter en epost med tittel (subject) og epost body med HTML-kode
+/// </summary>
+public static class EmailTemplates
+{
+    public static EmailBody SimpleText(string subject, string message) => new(
+        Subject: subject,
+        Html: $"<p>{message}</p>"
+    );
+}
