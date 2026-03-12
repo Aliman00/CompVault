96469867655016ff0bcc6e85a0acc1d3e14a0a52
lynsoftware <dev@lynsoftware.com>
2026-03-12 23:20:06 +0100

    feat: startet på tester, begynt og laget mocking

--- CompVault.Tests/Backend/Features/Auth/AuthService_RequestOtpAsync_Tests.cs
+++ CompVault.Tests/Backend/Features/Auth/AuthService_RequestOtpAsync_Tests.cs
@@ -1,6 +1,29 @@
-﻿namespace CompVault.Tests.Backend.Features.Auth;
+﻿using CompVault.Backend.Domain.Entities.Identity;
+using CompVault.Backend.Features.Auth;
+using CompVault.Backend.Features.Auth.Configuration;
+using CompVault.Backend.Infrastructure.Auth;
+using CompVault.Backend.Infrastructure.Email;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
 
-public class AuthService_RequestOtpAsync_Tests
+namespace CompVault.Tests.Backend.Features.Auth;
+
+public class AuthServiceRequestOtpAsyncTests
 {
-    
+    private readonly Mock<UserManager<ApplicationUser>> _userManagerMock;
+    private readonly Mock<ILogger<IAuthService>> _loggerMock;
+    private readonly Mock<IJwtService> _jwtServiceMock;
+    private readonly Mock<IOtpCodeService> _otpCodeServiceMock;
+    private readonly Mock<IEmailService> _emailServiceMock;
+
+    public AuthServiceRequestOtpAsyncTests()
+    {
+        // UserManager krever IUserStore i konstruktøren
+        var storeMock = new Mock<IUserStore<ApplicationUser>>();
+        _userManagerMock = new Mock<UserManager<ApplicationUser>>(
+            storeMock.Object, null!, null!, null!, null!, null!, null!, null!, null!);
+    }
+
 }
