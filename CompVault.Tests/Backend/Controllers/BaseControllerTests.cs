0359e5cccd86c8655d4dd415f1bdafb62567157a
lynsoftware <dev@lynsoftware.com>
2026-03-12 11:43:33 +0100

    feat: opprettet mappe og fil. lagd en TestableBaseController for å arve fra BaseController, og lagd første test

---
+++ CompVault.Tests/Backend/Controllers/BaseControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using CompVault.Backend.Controllers;
+using CompVault.Shared.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompVault.Tests.Backend.Controllers;
+
+/// <summary>
+/// Tester til basecontrolleren
+/// </summary>
+public class BaseControllerTests
+{
+    private readonly TestableBaseController _sut = new();
+    
+    // -------------------------------------------------------------------------
+    // HandleFailure<T>
+    // -------------------------------------------------------------------------
+    public void HandleFailureT_WhenResultIsSuccess_ThrowsInvalidOperationException()
+    {
+        // Arrange
+        Result<string> result = Result<string>.Success("test");
+        
+        // Act - Bruker Action for å kalle eventen InvokeHandleFailure med Result-objektet
+        Action act = () => _sut.InvokeHandleFailure(result);
+        
+        // Assert
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*resultat*");
+    }
+    
+}
+
+/// <summary>
+/// BaseController er abstract, så vi må opprette en controller vi kan teste på. Vi gjør det
+/// med å gjøre den public og arve fra BaseController
+/// </summary>
+public class TestableBaseController : BaseController
+{
+    // Wrapper metodene fra protected til Public
+    public ActionResult InvokeHandleFailure<T>(Result<T> result)
+        => HandleFailure(result);
+
+    public ActionResult InvokeHandleFailure(Result result)
+        => HandleFailure(result);
+}
