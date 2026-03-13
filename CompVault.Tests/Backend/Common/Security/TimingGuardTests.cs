4fe88591a441eaa4d8d6c4dac6ac6d70a4438881
Fredrik Magee <fredrikmagee@gmail.com>
2026-03-13 20:01:41 +0100

    feat: startet på TimingGuard-tester. 1 test ferdig

---
+++ CompVault.Tests/Backend/Common/Security/TimingGuardTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using CompVault.Backend.Common.Security;
+using FluentAssertions;
+
+namespace CompVault.Tests.Backend.Common.Security;
+
+public class TimingGuardTests
+{
+    /// <summary>
+    /// Tester at metoden delayer til mer enn 500 ms
+    /// </summary>
+    [Fact]
+    public async Task TimingGuard_MustDelay()
+    {
+        // Arrange - starter 2 stk StopWatch-objekter. 1 for å ta total tid, og en til metoden
+        var operationSw = Stopwatch.StartNew();
+        var totalSw = Stopwatch.StartNew();
+        
+        // Act
+        await TimingGuard.EnforceMinimumTimeAsync(operationSw, 500, CancellationToken.None);
+        totalSw.Stop();
+        
+        // Assert
+        totalSw.ElapsedMilliseconds.Should().BeGreaterThanOrEqualTo(500);
+    }
+}
