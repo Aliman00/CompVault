e1fc13ee2d5aebc5a6fe598ff0c383586cc276d8
lynsoftware <dev@lynsoftware.com>
2026-03-15 19:12:38 +0100

    feat: lagt appsettings.Testing.json i test-prosjektet. oppdatgert CompVault.Tests.csproj til å bruke apssettings.Testing.json

---
+++ CompVault.Tests/Backend/Integrations/BackendWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using CompVault.Backend.Infrastructure.Data;
+using CompVault.Backend.Infrastructure.Email;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace CompVault.Tests.Backend.Integrations;
+
+public class BackendWebApplicationFactory : WebApplicationFactory<Program>
+{
+    // Vi må eksponere EmailService for å sikre at vi ikke sender ekte eposter
+    public Mock<IEmailService> EmailServiceMock { get; } = new();
+
+    // protected override void ConfigureWebHot(IWebHostBuilder builder)
+    // {
+    //     builder.UseEnvironment("Testing");
+    //     
+    //     builder.ConfigureAppConfiguration(services =>
+    //     {
+    //         services.RemoveAll<DbContextOptions<AppDbContext>>();
+    //         services.AddDbContext<AppDbC
+    //     })
+    //     
+    // }
+}
