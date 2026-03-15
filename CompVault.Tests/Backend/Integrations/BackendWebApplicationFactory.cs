ef085a5d3614749b4e08163bf99e5072138f30e0
lynsoftware <dev@lynsoftware.com>
2026-03-15 19:22:39 +0100

    feat: satt opp en BackendWebApplicationFactory som bygger applikasjonen vår med variabler fra appsettings.Testing.json. Testet at applikasjonen kjører etter dette er lagt til

--- CompVault.Tests/Backend/Integrations/BackendWebApplicationFactory.cs
+++ CompVault.Tests/Backend/Integrations/BackendWebApplicationFactory.cs
@@ -3,25 +3,38 @@ using CompVault.Backend.Infrastructure.Email;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
 
 namespace CompVault.Tests.Backend.Integrations;
 
+/// <summary>
+/// Vi konfigurerer en WebApplicationFactory som starter hele Backend-applikasjonen vår InMemory
+/// </summary>
 public class BackendWebApplicationFactory : WebApplicationFactory<Program>
 {
-    // Vi må eksponere EmailService for å sikre at vi ikke sender ekte eposter
+    // Vi mocker EmailService for å mocke email kall
     public Mock<IEmailService> EmailServiceMock { get; } = new();
+    
+    /// <summary>
+    /// Overstyrer tjenester i Program.cs før applikasjonen starter.
+    /// Her fjerner vi  PostgreSQL-databasen og bruker InMemory
+    /// </summary>
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
 
-    // protected override void ConfigureWebHot(IWebHostBuilder builder)
-    // {
-    //     builder.UseEnvironment("Testing");
-    //     
-    //     builder.ConfigureAppConfiguration(services =>
-    //     {
-    //         services.RemoveAll<DbContextOptions<AppDbContext>>();
-    //         services.AddDbContext<AppDbC
-    //     })
-    //     
-    // }
+        builder.ConfigureServices(services =>
+        {
+            // Vi bytter ut PostgreSQL med InMemory-database
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+
+            // Bytter ut den ekte EmailService med mocken
+            services.RemoveAll<IEmailService>();
+            services.AddScoped<IEmailService>(_ => EmailServiceMock.Object);
+        });
+    }
 }
