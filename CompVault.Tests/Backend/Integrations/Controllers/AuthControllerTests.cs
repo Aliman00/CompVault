f6487714fd968349f8e31048a751cf522089de7c
lynsoftware <dev@lynsoftware.com>
2026-03-15 20:37:31 +0100

    feat: lagd en CreateApplicaitonUser-metode for å opprette ApplicationUser. Brukes i flere tester

---
+++ CompVault.Tests/Backend/Integrations/Controllers/AuthControllerTests.cs
@@ -0,0 +1,13 @@
+﻿namespace CompVault.Tests.Backend.Integrations.Controllers;
+
+/// <summary>
+/// IClassFixture sikrer at vi ikke oppretter flere instanser av applikasjonen
+/// Siden xUnit-konstruktøren kan ikke være async så oppretter vi to hooks per test:
+/// En InitializeAsync som kjører for hver test, og en DisposeAsync for å rydde opp
+/// </summary>
+public class AuthControllerTests : IClassFixture<BackendWebApplicationFactory>, IAsyncLifetime
+{
+    private readonly BackendWebApplicationFactory _factory;
+    private readonly HttpClient _client;
+    
+}
---
+++ CompVault.Tests/Common/TestDataSeeder.cs
@@ -0,0 +1,25 @@
+﻿using CompVault.Backend.Domain.Entities.Identity;
+
+namespace CompVault.Tests.Common;
+
+public class TestDataSeeder
+{
+    /// <summary>
+    /// Oppretter en ApplicationUser for testing. Brukes i de fleste testene.
+    /// Hvis deletedAt har en verdi, så er brukeren inaktive/slettet
+    /// </summary>
+    /// <param name="email">Optional string med Epost for å opprette forskjellige brukere</param>
+    /// <param name="deletedAt">DateTime som bestemmer om brukeren er aktive/slettet</param>
+    /// <returns>En ferdig opprettet ApplicationUser for testing</returns>
+    public static ApplicationUser CreateApplicationUser(string email = "test@compvault.no", DateTime? deletedAt = null)
+        => new()
+    {
+        Id = Guid.NewGuid(),
+        Email = email,
+        UserName = email,
+        FirstName = "Fredrik",
+        LastName = "Magee",
+        IsActive = deletedAt == null,
+        DeletedAt = deletedAt
+    };
+}
