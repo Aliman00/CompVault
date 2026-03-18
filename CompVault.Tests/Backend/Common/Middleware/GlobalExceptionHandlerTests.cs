40ead21d9debb45da4c33c6fd650fdebed2e1244
lynsoftware <dev@lynsoftware.com>
2026-03-18 18:44:19 +0100

    feat: Opprettet GlobalExceptionHandlerTests og lagt til at vi kan dele internal til Test i CSPROJ-filen

---
+++ CompVault.Tests/Backend/Common/Middleware/GlobalExceptionHandlerTests.cs
@@ -0,0 +1,14 @@
+﻿namespace CompVault.Tests.Backend.Common.Middleware;
+
+public class GlobalExceptionHandlerTests
+{
+    private readonly GlobalExce
+    
+    [Theory]
+    public async Task GlobalExceptionHandler_DifferentExceptionType_ReturnsCorrectResponse(
+        Type exceptionType, int expectedStatus, string expectedCode)
+    {
+        // Arrange
+        var logger = Substitute.
+    }
+}
