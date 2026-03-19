1c1c2d957fe647ab2b69c4ae2f1a18253429e423
lynsoftware <dev@lynsoftware.com>
2026-03-19 12:17:37 +0100

    feat: Opprettet en BaseRepo-klasse for InMemory-testing. Gjør det nkelt å sette opp og seede en InMemory-database til hvert repo

---
+++ CompVault.Tests/Backend/Infrastructure/Repositories/InMemoryRepositoryBase.cs
@@ -0,0 +1,60 @@
+﻿using CompVault.Backend.Domain.Entities.Auth;
+using CompVault.Backend.Domain.Entities.Identity;
+using CompVault.Backend.Infrastructure.Data;
+using CompVault.Tests.Common;
+using CompVault.Tests.Common.Constants;
+using Microsoft.EntityFrameworkCore;
+namespace CompVault.Tests.Backend.Infrastructure.Repositories;
+
+/// <summary>
+/// Baseklasse for repoet. Slipper å sette opp InMemoryDatabase manuelt på hver eneste repository-test
+/// </summary>
+public abstract class InMemoryRepositoryBase : IDisposable
+{
+    protected readonly AppDbContext Context;
+
+    protected InMemoryRepositoryBase()
+    {
+        // Setter opp InMemoryDatabase
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        
+        Context = new AppDbContext(options);
+    }
+    
+    /// <summary>
+    /// Seeder en bruker og lagrer den i InMemory-databasen
+    /// </summary>
+    /// <param name="email">Default epost, eller en annen epost hvis det er en annen bruker</param>
+    /// <returns>Den opprettede brukeren hvis egenskaper (som ID) er nødvendig for testing</returns>
+    protected async Task<ApplicationUser> SeedUserAsync(string email = TestConstants.Users.DefaultEmailForActiveUser)
+    {
+        var user = TestDataSeeder.CreateApplicationUser(email: email);
+        Context.Users.Add(user);
+        await Context.SaveChangesAsync();
+        return user;
+    }
+    
+    /// <summary>
+    /// Hjelpemetode for å seede en OtpCodeAsync i databasen for en bruker. Den har default verdier til en
+    /// ikke-eksisterende OtpCode, men parameter for å endre koden
+    /// </summary>
+    /// <param name="userId">Bruker ID til brukeren som får en OtpCode</param>
+    /// <param name="isUsed">Koden er enten used eller ikke</param>
+    /// <param name="expiresAt">Koden har enten en utgåttende tid eller ingen</param>
+    /// <param name="createdAt">Koden har en opprettet tid hvis den eksisterer, eller så har den ikke det</param>
+    /// <returns>En eksisterende OtpCode</returns>
+    protected async Task<OtpCode> SeedOtpCodeAsync(Guid userId, bool isUsed = false,
+        DateTime? expiresAt = null, DateTime? createdAt = null)
+    {
+        var otpCode = TestDataSeeder.CreateOtpCode(userId: userId, isUsed: isUsed,
+            expiresAt: expiresAt, createdAt: createdAt);
+
+        Context.Set<OtpCode>().Add(otpCode);
+        await Context.SaveChangesAsync();
+        return otpCode;
+    }
+    
+    public void Dispose() => Context.Dispose();
+}
