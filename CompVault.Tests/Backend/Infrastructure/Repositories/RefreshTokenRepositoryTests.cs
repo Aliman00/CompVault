d8c37062dd7a093f87483f130368a85a92c0b774
lynsoftware <dev@lynsoftware.com>
2026-03-19 13:16:34 +0100

    feat: laget tester til GetValidTokenAsync

--- CompVault.Tests/Backend/Infrastructure/Repositories/RefreshTokenRepositoryTests.cs
+++ CompVault.Tests/Backend/Infrastructure/Repositories/RefreshTokenRepositoryTests.cs
@@ -1,9 +1,7 @@
-﻿using CompVault.Backend.Domain.Entities.Auth;
-using CompVault.Backend.Domain.Entities.Identity;
-using CompVault.Backend.Infrastructure.Data;
-using CompVault.Backend.Infrastructure.Repositories.Auth;
-using CompVault.Tests.Common;
-using Microsoft.EntityFrameworkCore;
+﻿using CompVault.Backend.Infrastructure.Repositories.Auth;
+using FluentAssertions;
+
+
 
 namespace CompVault.Tests.Backend.Infrastructure.Repositories;
 
@@ -16,5 +14,96 @@ public class RefreshTokenRepositoryTests : InMemoryRepositoryBase
         _sut = new RefreshTokenRepository(Context);
     }
     
+    // -------------------------------------------------------------------------
+    // GetValidTokenAsync
+    // -------------------------------------------------------------------------
     
+    /// <summary>
+    /// Tester at en valid refresh token eksisterer
+    /// </summary>
+    [Fact]
+    public async Task GetValidTokenAsync_TokenExists_ReturnsToken()
+    {
+        // Arrange
+        var user = await SeedUserAsync();
+        var refreshToken = await SeedRefreshTokenAsync(userId: user.Id);
+        
+        // Act
+        var validRefreshToken = await _sut.GetValidTokenAsync(refreshToken.Token);
+        
+        // Assert
+        validRefreshToken.Should().NotBeNull();
+        validRefreshToken.Id.Should().Be(refreshToken.Id);
+    }
+    
+    /// <summary>
+    /// Tester at innsendt token gir null
+    /// </summary>
+    [Fact]
+    public async Task GetValidTokenAsync_TokenDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        var token = "testtoken";
+        
+        // Act
+        var storedRefreshToken = await _sut.GetValidTokenAsync(token);
+        
+        // Assert
+        storedRefreshToken.Should().BeNull();
+    }
+    
+    /// <summary>
+    /// Tester at en revoked token returnerer null
+    /// </summary>
+    [Fact]
+    public async Task GetValidTokenAsync_TokenIsRevoked_ReturnsNull()
+    {
+        // Arrange
+        var user = await SeedUserAsync();
+        var refreshToken = await SeedRefreshTokenAsync(userId: user.Id, isRevoked: true);
+        
+        // Act
+        var storedRefreshToken = await _sut.GetValidTokenAsync(refreshToken.Token);
+        
+        // Assert
+        storedRefreshToken.Should().BeNull();
+    }
+    
+    /// <summary>
+    ///  Tester at utgått token returnerer null
+    /// </summary>
+    [Fact]
+    public async Task GetValidTokenAsync_TokenIsExpired_ReturnsNull()
+    {
+        // Arrange
+        var user = await SeedUserAsync();
+        var refreshToken = await SeedRefreshTokenAsync(userId: user.Id, expiresAt: DateTime.UtcNow.AddMinutes(-30));
+        
+        // Act
+        var storedRefreshToken = await _sut.GetValidTokenAsync(refreshToken.Token);
+        
+        // Assert
+        storedRefreshToken.Should().BeNull();
+    }
+    
+    /// <summary>
+    /// Tester at vi riktig token blir hentet når det eksisterer flere tokens i databasen
+    /// </summary>
+    [Fact]
+    public async Task GetValidTokenAsync_MutipleValidTokenExists_ReturnsCorrectToken()
+    {
+        // Arrange - Seeder bruker og token for to forskjellige brukere
+        var userA = await SeedUserAsync();
+        var refreshTokenUserA = await SeedRefreshTokenAsync(userId: userA.Id);
+        
+        var userB = await SeedUserAsync(email: "userb@compvault.com");
+        await SeedRefreshTokenAsync(userId: userB.Id);
+        
+        // Act
+        var validRefreshToken = await _sut.GetValidTokenAsync(refreshTokenUserA.Token);
+        
+        // Assert
+        validRefreshToken.Should().NotBeNull();
+        validRefreshToken.Id.Should().Be(refreshTokenUserA.Id);
+    }
 }
