531d600a11e8d8f202c22c5f1e7cfaa452c527bb
lynsoftware <dev@lynsoftware.com>
2026-03-24 16:06:46 +0100

    feat: lagt til AuthService med tilhørende interface. Lagd ServiceCollectionExtensions som oppretter en HttpClient med tilkobling opp mot backend. I samme fil har jeg lagt til en AddFrontServices som registerer IAuthSrevice og bruker begge i Program.cs

---
+++ CompVault.Frontend/Features/Auth/Services/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using CompVault.Shared.DTOs.Auth;
+using CompVault.Shared.Result;
+
+namespace CompVault.Frontend.Features.Auth.Services;
+
+public interface IAuthService
+{
+    /// <summary>
+    /// API-kall med RequestOtpRequest for å genere en OTP-kode til brukeren som skal logge inn. Frontend
+    /// navigerer til neste side, mens backend oppretter en OTP-kode og sender utifra ønsket leveringsmetode
+    /// </summary>
+    Task<Result> RequestOtpAsync(RequestOtpRequest request, CancellationToken ct);
+}
