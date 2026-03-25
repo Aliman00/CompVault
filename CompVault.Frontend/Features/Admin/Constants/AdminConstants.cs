a87349783cce1b2934b206528f06cd86f79154a6
lynsoftware <dev@lynsoftware.com>
2026-03-25 10:40:45 +0100

    feat: lagt til en recentactivityswidget og oppdaterte AdminDashboard til å bruke denne

--- CompVault.Frontend/Features/Admin/Constants/AdminConstants.cs
+++ CompVault.Frontend/Features/Admin/Constants/AdminConstants.cs
@@ -5,6 +5,7 @@ public static class AdminConstants
     public const string TotalUsers = "Aktive brukere i bedriften";
     public const string TotalRoles = "Totale roller opprettet i bedriften";
     public const string TotalDepartments = "Totale avdelinger i bedriften";
+    public const string RecentActivity = "Siste aktivitet";
     
     public const string StorageKey = "compvault-admin-widgets";
 }
