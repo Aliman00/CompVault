7d628614b8ac49856b003bd5166fbe4301ea7902
Alm1n <almin.dev@pm.me>
2026-04-01 22:42:46 +0200

    feat: RBAC migrations

---
+++ CompVault.Backend/Migrations/20260401204140_RoleBasedAccessControl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompVault.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleBasedAccessControl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSystem",
+                table: "AspNetRoles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_DeletedAt",
+                table: "AspNetUsers",
+                column: "DeletedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_DeletedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsSystem",
+                table: "AspNetRoles");
+        }
+    }
+}
