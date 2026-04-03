fb0704e34b0548cb552b8e92d57a11676ec78515
lynsoftware <dev@lynsoftware.com>
2026-04-03 23:48:46 +0200

    chore: ryddet opp i en kommentar, endret en page til å være små bokstaver som de andre og fjernet en tom kodeblokk

--- CompVault.Frontend/Common/Services/IThemeService.cs
+++ CompVault.Frontend/Common/Services/IThemeService.cs
@@ -8,7 +8,7 @@ public interface IThemeService
     /// Henter valgt lyst/mørkt modus ved å først sjekke localstorage eller fallback til nettleseren sin modus
     /// </summary>
     /// <param name="provider">MudThemeProvider som styrer fargene til hver modus</param>
-    /// <returns>True hvis dar kmode, false hvis light mode</returns>
+    /// <returns>True hvis dark mode, false hvis light mode</returns>
     Task<bool> GetInitialDarkModeAsync(MudThemeProvider provider);
     
     /// <summary>
