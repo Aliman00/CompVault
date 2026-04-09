ac9c15317af13cec244a30558209f914b962e6e5
lynsoftware <dev@lynsoftware.com>
2026-04-09 21:02:47 +0200

    fix: fjernet en ekstra docker compose fil og allerede eksisterende logikk i program.cs

--- CompVault.Frontend/Program.cs
+++ CompVault.Frontend/Program.cs
@@ -1,53 +1,40 @@
 using CompVault.Frontend;
 using CompVault.Frontend.Extensions;
-using CompVault.Frontend.Features.Auth.Services;
-
 using MudBlazor.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// ====================== Services ======================
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.AddSerilogLogging();
 builder.Services.AddHttpClients(builder.Configuration);
 builder.Services.AddMudServices();
-
-// ---------- Auth / OTP ----------
 builder.Services.AddAuth(builder.Configuration, builder.Environment);
 builder.Services.AddAuthPolicies();
-builder.Services.AddScoped<AuthService>(); 
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddHttpContextAccessor();// <-- Legg til AuthService som scoped
-
 builder.Services.AddFrontendServices(builder.Environment);
 builder.Services.AddRazorPages();
 
-// ====================== Build app ======================
 WebApplication app = builder.Build();
 
-// ====================== Middleware ======================
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 app.UseAntiforgery();
 
-// ============ Autentisering ============
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapRazorPages();
+
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
-
 
 app.Run();
