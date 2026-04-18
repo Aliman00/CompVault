98924b4fc3b0813a159537c430e839a48aa1c5a2
lynsoftware <dev@lynsoftware.com>
2026-04-19 00:48:56 +0200

    feat: stillingstittel i frontend er ferdig

---
+++ CompVault.Frontend/Features/JobTitle/Models/CreateJobTitleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CompVault.Shared.Constants.Validations;
+using CompVault.Shared.DTOs.JobTitles;
+namespace CompVault.Frontend.Features.JobTitle.Models;
+
+public class CreateJobTitleModel
+{
+    [Required(ErrorMessage = JobTitleValidations.Errors.NameRequired)]
+    [MaxLength(JobTitleValidations.NameMaxLength, ErrorMessage = JobTitleValidations.Errors.NameMaxLength)]
+    public string Name { get; set; } = string.Empty;
+
+    public CreateJobTitleRequest ToRequest() => new() { Name = Name };
+}
