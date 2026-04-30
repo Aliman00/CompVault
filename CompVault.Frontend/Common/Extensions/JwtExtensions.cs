11b2359b6125698f0a2557a0a08151acaa0886d9
lynsoftware <dev@lynsoftware.com>
2026-04-30 14:33:23 +0200

    feat: fikset og oppdatert login-sidene. Gjort det samme med dev-panel

---
+++ CompVault.Frontend/Common/Extensions/JwtExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace CompVault.Frontend.Common.Extensions;
+
+public static class JwtExtensions
+{   
+    /// <summary>
+    /// Parser claims fra JWT for å legge cookies inn i nettleseren
+    /// </summary>
+    /// <param name="jwt"></param>
+    /// <returns></returns>
+    public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        // JWT er 3 stk base64url-segmenter som separeres med en punkt - vi skal ha midterste som blir kalt payload
+        string base64UrlPayload = jwt.Split('.')[1];
+
+        // Konverterer til vanlig base64 og fjerner padding som base64url fjernet
+        // Vi må gjøre dette for at Conver.FromBase64String skal kunne det om til json
+        string standardBase64 = base64UrlPayload.Replace('-', '+').Replace('_', '/');
+        string paddedBase64 = standardBase64.PadRight(standardBase64.Length +
+                                                      (4 - standardBase64.Length % 4) % 4, '=');
+        // Base64 gjøres om til JSON for å kunne deserialiseres, slik at vi får en ordbok med verdiene fra JWT-en
+        string json = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(paddedBase64));
+        Dictionary<string, JsonElement>? parsed = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json)!;
+
+        // Vi returnerer en liste med Claims, hentet fra nøkkelparet i ordboka. Vi har lagt til at vi håndterer claims
+        // som er arrays (feks Roller)
+        var claims = new List<Claim>();
+        foreach (KeyValuePair<string, JsonElement> kv in parsed)
+        {
+            if (kv.Value.ValueKind == JsonValueKind.Array)
+            {
+                claims.AddRange(kv.Value.EnumerateArray()
+                    .Select(element => new Claim(kv.Key, element.ToString())));
+            }
+            else
+            {
+                claims.Add(new Claim(kv.Key, kv.Value.ToString()));
+            }
+        }
+
+        return claims;
+    }
+}
--- CompVault.Frontend/Common/Pages/LoginCallback.cshtml.cs
+++ CompVault.Frontend/Common/Pages/LoginCallback.cshtml.cs
@@ -1,53 +1,41 @@
 ﻿using System.Security.Claims;
-
 using CompVault.Frontend.Common.Configuration;
 using CompVault.Frontend.Common.Extensions;
-using CompVault.Frontend.Features.Auth.Services;
 using CompVault.Shared.DTOs.Auth;
-using CompVault.Shared.Result;
-
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace CompVault.Frontend.Common.Pages;
 
 /// <summary>
 /// Modellen til en SSR-side som lar oss legge til token i nettlesere - JS-scriptet submitter som gjør at
 /// OnPostAsync blir kjørt
 /// </summary>
-public class LoginCallback(IAuthService authService, AuthSettings authSettings, IWebHostEnvironment env) : PageModel
+public class LoginCallback(AuthSettings authSettings, IWebHostEnvironment env) : PageModel
 {
     [BindProperty]
-    public VerifyOtpRequest OtpRequest { get; set; } = new();
+    public TokenResponse TokenResponse { get; set; } = new();
 
     /// <summary>
     /// Sender VerifyOtpRequest til backend og legger til en cookie i nettleseren.
-    /// Redirecter til Login-siden hvis noe failer TODO: Må implementeres i Login-page
+    /// Redirecter til Login-siden hvis noe failer 
     /// </summary>
     public async Task<IActionResult> OnPostAsync()
     {
-        if (!ModelState.IsValid)
-            return LocalRedirect("/login?error=invalid");
-
-        Result<(ClaimsPrincipal Principal, TokenResponse Tokens)> verifyOtpResult =
-            await authService.VerifyOtpAsync(OtpRequest, HttpContext.RequestAborted);
-
-        if (verifyOtpResult.IsFailure)
-            return LocalRedirect("/login?error=invalid");
-
-        (ClaimsPrincipal? principal, TokenResponse? tokens) = verifyOtpResult.Value;
-
-        if (principal.Identity is not ClaimsIdentity identity)
-            return LocalRedirect("/login?error=invalid");
-
-        // Setter opp auth-cookie først, deretter RefreshToken-cookie
-        identity.AddClaim(new Claim("access_token", tokens.AccessToken));
+        if (string.IsNullOrWhiteSpace(TokenResponse.AccessToken) ||
+            string.IsNullOrWhiteSpace(TokenResponse.RefreshToken))
+            return LocalRedirect("/login-email?error=invalid");
+        
+        IEnumerable<Claim> claims = JwtExtensions.ParseClaimsFromJwt(TokenResponse.AccessToken);
+        var identity = new ClaimsIdentity(claims, "jwt");
+        identity.AddClaim(new Claim("access_token", TokenResponse.AccessToken));
+        var principal = new ClaimsPrincipal(identity);
+        
         await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal,
             new AuthenticationProperties { IsPersistent = true });
 
-        HttpContext.AppendRefreshTokenCookie(tokens.RefreshToken, authSettings, env);
+        HttpContext.AppendRefreshTokenCookie(TokenResponse.RefreshToken, authSettings, env);
 
         return LocalRedirect("/");
     }
